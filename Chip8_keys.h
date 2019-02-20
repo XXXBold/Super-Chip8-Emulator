66d67b65b86db75f7ef00c50ade93b3cad20361a
XXXBold <30773753+XXXBold@users.noreply.github.com>
2019-02-20 20:46:36 +0000

    Delete Chip8_keys.h

--- Chip8_keys.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHIP8_KEYS_H_INCLUDED
-  #define CHIP8_KEYS_H_INCLUDED
-
-void vChip8_Keys_GetState_g(unsigned short *pusKeyMap);
-unsigned char ucChip8_Keys_WaitForNext_g(void);
-
-#endif /* CHIP8_KEYS_H_INCLUDED */
